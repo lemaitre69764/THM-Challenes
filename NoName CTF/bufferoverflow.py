9a3994f9193b90580b5ebcca08c738a482f7e380
madi <madimirzakup@gmail.com>
2025-02-11 19:08:16 +0500

    #

--- NoName CTF/bufferoverflow.py
+++ NoName CTF/bufferoverflow.py
@@ -1,4 +1,18 @@
 import telnetlib
 import argparse
 
-parser = 
+parser = argparse.ArgumentParser(description="BOF Exploit")
+parser.add_argument("host", help="The host IP address")
+parser.add_argument("port", help="The host port")
+arsg = parser.parse_args()
+
+"""
+read & write
+"""
+def read(end_text):
+    tn.read_until(end_text.encode())
+
+def write(text):
+    tn.write(("{0}\n".format(text)).encode())
+
+#connect 
